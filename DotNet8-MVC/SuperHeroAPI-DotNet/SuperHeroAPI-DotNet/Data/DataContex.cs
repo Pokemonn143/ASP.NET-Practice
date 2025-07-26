6dfc6188d7e8b02255a679d543e2d3b0c8562f76
Pokemonn143 <geraldcury@hotmail.com>
2025-07-26 10:03:16 -0400

    Added Context and connection string

---
+++ DotNet8-MVC/SuperHeroAPI-DotNet/SuperHeroAPI-DotNet/Data/DataContex.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI_DotNet.Entities;
+
+namespace SuperHeroAPI_DotNet.Data
+{
+    public class DataContex: DbContext
+    {
+
+        public DataContex(DbContextOptions<DataContex> options) :base(options)
+        {
+            
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+
+    }
+}
--- DotNet8-MVC/SuperHeroAPI-DotNet/SuperHeroAPI-DotNet/Entities/SuperHero.cs
+++ DotNet8-MVC/SuperHeroAPI-DotNet/SuperHeroAPI-DotNet/Entities/SuperHero.cs
@@ -10,5 +10,7 @@
         public string LastName { get; set; } = string.Empty;
 
         public string place { get; set; } = string.Empty;
+
+
     }
 }
