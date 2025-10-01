e7de62f658848e01d1cde4bc073276cc6da0b30b
Pokemonn143 <geraldcury@hotmail.com>
2025-10-01 11:59:35 -0400

    added oauth2 and auth to weatherforescast

--- DotNet8-MVC/SuperHeroAPI-DotNet/SuperHeroAPI-DotNet/Program.cs
+++ DotNet8-MVC/SuperHeroAPI-DotNet/SuperHeroAPI-DotNet/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using SuperHeroAPI_DotNet.Data;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +12,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options=>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
 builder.Services.AddDbContext<DataContex>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
